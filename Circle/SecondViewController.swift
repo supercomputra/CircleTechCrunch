1e996a59dcb648cac1f19026821cd759dceda14a
Zulwiyoza Putra <zulwiyozaputra@yahoo.com>
2016-09-15 20:38:32 -0700

    Initial Commit

---
+++ Circle/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Circle
+//
+//  Created by Zulwiyoza Putra on 9/15/16.
+//  Copyright © 2016 Zulwiyoza Putra. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
