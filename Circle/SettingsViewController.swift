98be8bc7dfbe4111db6eeceb09cd3a4f556f5129
Zulwiyoza Putra <zulwiyozaputra@yahoo.com>
2016-09-16 11:56:14 -0700

    terbhaeq

--- Circle/SettingsViewController.swift
+++ Circle/SettingsViewController.swift
@@ -20,17 +20,5 @@ class SettingViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-
-
 }
 
